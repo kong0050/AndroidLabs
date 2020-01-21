3a1a5849bd76cc34350f59db3919ac215ee31ea8
kong0050 <kongxiaoting3155@gmail.com>
2020-01-21 15:27:56 -0500

    Added 2 features for Lab 2 and listeners version2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -47,14 +47,14 @@ public class MainActivity extends AppCompatActivity {
                 isChecked = check.isChecked();
                 String s = "";
                 if(isChecked==true){
-                    s="on";
-                }else{s="off";}
-                Snackbar snackbar = Snackbar.make(check, "Checkbox is "+s, Snackbar.LENGTH_LONG)
-                        .setAction("UNDO", new View.OnClickListener() {
+                    s=(MainActivity.this).getResources().getString(R.string.on);
+                }else{s=(MainActivity.this).getResources().getString(R.string.off);}
+                Snackbar snackbar = Snackbar.make(check, (MainActivity.this).getResources().getString(R.string.checkbox1)+" "+s, Snackbar.LENGTH_LONG)
+                        .setAction((MainActivity.this).getResources().getString(R.string.undo), new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
                                 check.setChecked(!check.isChecked());
-                                Snackbar snackbar1 = Snackbar.make(check, "Restored!", Snackbar.LENGTH_LONG);
+                                Snackbar snackbar1 = Snackbar.make(check, (MainActivity.this).getResources().getString(R.string.restore), Snackbar.LENGTH_LONG);
                                 snackbar1.show();
                                 //remember to change the status of checked true to false!!!!!!! can get back to the original status
                             }
@@ -70,14 +70,14 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
                 String s = "";
                 if(isChecked){
-                    s="on";
-                }else{s="off";}
-                Snackbar snackbar = Snackbar.make(switcha, "Switch is "+s, Snackbar.LENGTH_LONG)
-                        .setAction("UNDO", new View.OnClickListener() {
+                    s=(MainActivity.this).getResources().getString(R.string.on);
+                }else{s=(MainActivity.this).getResources().getString(R.string.off);}
+                Snackbar snackbar = Snackbar.make(switcha, (MainActivity.this).getResources().getString(R.string.switch1)+" "+s, Snackbar.LENGTH_LONG)
+                        .setAction((MainActivity.this).getResources().getString(R.string.undo), new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
                                 switcha.setChecked(!isChecked);
-                                Snackbar snackbar1 = Snackbar.make(check, "Restored!", Snackbar.LENGTH_LONG);
+                                Snackbar snackbar1 = Snackbar.make(check, (MainActivity.this).getResources().getString(R.string.restore), Snackbar.LENGTH_LONG);
                                 snackbar1.show();
                                 //remember to change the status of checked true to false!!!!!!! can get back to the original status
                             }
