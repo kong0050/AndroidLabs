2f43a783136d30505bae2ee594a0dd655e56212b
kong0050 <kongxiaoting3155@gmail.com>
2020-01-28 20:18:10 -0500

    lab3 version1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,7 @@ import static android.widget.CompoundButton.*;
 public class MainActivity extends AppCompatActivity {
 
     EditText mEmailView ;
-//    EditText  mPasswordView = (EditText)findViewById(R.id.password);
+    EditText  mPasswordView;
 
 
 //    String password = mPasswordView.getText().toString();
@@ -36,14 +37,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_lab3_);
 
          mEmailView = (EditText)findViewById(R.id.email);
-//    EditText  mPasswordView = (EditText)findViewById(R.id.password);
+         mPasswordView = (EditText)findViewById(R.id.password);
+
+//        String email = mEmailView.getText().toString();
+//        String password = mPasswordView.getText().toString();
 
         final Button btn = (Button)findViewById(R.id.button);
         loadLogoutInformation();
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                startProfileActivity();
+                    startProfileActivity();
             }
         });
     }
@@ -66,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = context.getSharedPreferences("LoginEmail", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("Email", emailx);
-//        editor.putString("Password", password);
         editor.commit();
     }
 
